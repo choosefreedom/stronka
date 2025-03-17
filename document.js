1b4a72dadeede61f22cd27b9fd76d97093825a77
Mimi <106749479+choosefreedom@users.noreply.github.com>
2025-03-18 00:26:12 +0100

    poprawki2

--- document.js
+++ document.js
@@ -22,3 +22,27 @@ document.querySelectorAll('.item').forEach(item => {
         }
     });
 });
+
+document.querySelectorAll('.item_col').forEach(item => {
+    item.addEventListener('click', function () {
+        const content = this.querySelector('.content');
+        const decorImg = this.querySelector('.decorator_img');
+        const decorHide = this.querySelector('.decor_hide');
+
+
+        content.classList.toggle('open');
+
+
+        this.classList.toggle('open');
+
+
+        if  (decorImg.style.display === 'none') {
+            console.log("dupa")
+            decorImg.style.display = 'block';
+            decorHide.style.display = 'none';
+        } else {
+            decorImg.style.display = 'none';
+            decorHide.style.display = 'block';
+        }
+    });
+});
