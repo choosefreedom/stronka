f28d790a310b67dbceddc4243f81f8157233c61f
Mimi <106749479+choosefreedom@users.noreply.github.com>
2025-03-18 17:44:44 +0100

    js i inne

--- document.js
+++ document.js
@@ -13,7 +13,6 @@ document.querySelectorAll('.item').forEach(item => {
 
 
         if  (decorImg.style.display === 'none') {
-            console.log("dupa")
             decorImg.style.display = 'block';
             decorHide.style.display = 'none';
         } else {
@@ -46,3 +45,39 @@ document.querySelectorAll('.item_col').forEach(item => {
         }
     });
 });
+
+function onSizeChange() {
+    const element = document.getElementById('change2');
+    const element2 = document.getElementById('change1');
+
+    const isSmallScreen = window.matchMedia("(max-width: 1120px)").matches;
+
+    // setElementState(element, isSmallScreen);
+    // setElementState(element2, !isSmallScreen);
+
+    if (window.matchMedia("(max-width: 1120px)").matches) {
+       setElementState(element,true);
+       setElementState(element2, false);
+
+    } else {
+        setElementState(element,false);
+        setElementState(element2,true);
+    }
+}
+function setElementState(element,active = false) {
+    if (active) {
+        element.classList.add('item_col');
+        element.classList.remove('item');
+    }
+    else {
+        element.classList.add('item');
+        element.classList.remove('item_col');
+    }
+}
+
+// Nasłuchujemy zmian rozmiaru ekranu
+window.addEventListener('resize', onSizeChange);
+// Wywołujemy funkcję przy załadowaniu strony
+onSizeChange();
+
+
