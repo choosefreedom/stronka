42cde69e3e42fb56220a5d37084238da27e4a2eb
Mimi <106749479+choosefreedom@users.noreply.github.com>
2025-03-18 21:59:10 +0100

    html remove

--- menu.js
+++ menu.js
@@ -1,23 +1,24 @@
 
-
+// menu
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
         nav = document.getElementById(navId);
 
     toggle.addEventListener('click', () => {
-        // Dodaj/usuń klasę do menu
+
         nav.classList.toggle('show-menu');
 
-        // Dodaj/usuń klasę do ikony
+
         toggle.classList.toggle('show-icon');
     });
 }
 
-// Wywołanie funkcji, przekazując odpowiednie id elementów
+
 showMenu('menuicon', 'nav-menu');
 
-// indexhtml
-if (window.location.pathname.endsWith("index.html")) {
-    window.history.replaceState(null, "", window.location.pathname.replace("index.html", ""));
-}
-
+document.addEventListener('DOMContentLoaded', function () {
+    if (window.location.pathname.endsWith('.html')) {
+        const newPath = window.location.pathname.replace('.html', '');
+        window.history.replaceState({}, '', newPath);
+    }
+});
